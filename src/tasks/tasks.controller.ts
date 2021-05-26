cc0834be840fe937ccdeaa3cb8c5c30b9f115ece
Mateus Nascimento <mateus.msnascimento@gmail.com>
2021-05-26 13:32:58 -0300

    feat: add updatetask to task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
-import { threadId } from 'worker_threads';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -32,5 +39,13 @@ export class TasksController {
   @Delete('/:id')
   deleteTask(@Param('íd') id: string): void {
     this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
